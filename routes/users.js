f453c5e9db7ae3c64cb9d4efb9054a19173a4ecb
vigneshwarank00 <smilevicky2000@gmail.com>
2022-07-15 21:27:38 +0530

    change user comments

--- routes/users.js
+++ routes/users.js
@@ -2,25 +2,25 @@ const express = require('express')
 const router = express.Router()
 const mongo = require('../helper')
 
-// read posts
+// read user
 router.get('/', async (req, res) => {
   const users = await mongo.db.collection('users').find().toArray()
   res.send(users)
 })
-// create post
+// create user
 router.post('/', async (req, res) => {
   const user = req.body
   const users = await mongo.db.collection('users').insertMany(user)
   res.send(users)
 })
-// update post
+// update user
 router.put('/:id', async (req, res) => {
   const id = req.params.id
   const user = req.body
   const users = await mongo.db.collection('users').updateOne({ id: +id }, { $set: user })
   res.send(users)
 })
-// delete post
+// delete user
 router.delete('/:id', async (req, res) => {
   const id = req.params.id
   const users = await mongo.db.collection('users').deleteOne({ id: +id })
